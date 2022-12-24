8ac742380ab0a9b5aa9fc7557b4a5adfdce7dbfd
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2022-12-24 12:05:55 +0200

    add implementation of other operations

--- src/calc.js
+++ src/calc.js
@@ -5,6 +5,15 @@ function calc(num1, num2, operation) {
         case '+':
             result = num1 + num2;
             break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
     }
 
     return result;
