c231d2ebf20c207c3563e50faf848fcb672f23de
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2022-12-24 12:00:15 +0200

    start implementing calculator

---
+++ src/calc.js
@@ -0,0 +1,11 @@
+function calc(num1, num2, operation) {
+    let result = 0;
+
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+    }
+
+    return result;
+}
