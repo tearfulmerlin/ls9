aaecdd05f0d28add3cd7401d48181b96d07a7553
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2022-12-24 11:50:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,96 @@
+// FLASHBACK
+
+// functoins
+// showMessage();
+
+// const showMessage = function() {}; // - expression
+// function showMessage(){}; // - declaration
+
+/******* JSON *******/
+// Purpose
+// Data represetation
+// used in APIs
+// lightweight, easy to read
+// prog langs support
+// valid js
+
+// Types
+// string
+// number
+// boolean
+// null
+// array
+// object
+
+// const userJohn = {
+//     "name": "John",
+//     "friends": []
+// }
+
+// const userMike = {
+//     "name": "Mike",
+//     "age": 28,
+//     "isAdmin": false,
+//     "tel": null,
+//     "friends": [
+//         {
+//             "name": "Pete"
+//         }
+//     ],
+//     run: function() {
+//         console.log('running');
+//     }
+// };
+
+// userJohn.friends.push(userMike);
+// userMike.friends.push(userJohn);
+
+
+
+// console.log(userMike);
+// console.log(userJohn);
+
+// const stringified = JSON.stringify(userMike);
+// console.log(stringified);
+
+// const parsed = JSON.parse(stringified);
+// console.log(parsed);
+
+
+// parse and stringify
+
+
+
+
+
+
+/******* Node & NPM & linter *******/
+// package.json
+// init
+// install
+// node_modules
+
+
+
+
+/******* GIT *******/
+// control version system
+/* repositiory */
+// branches
+// commits
+
+// clone url
+// init
+// status
+// add
+// commit
+// push
+// pull
+// branch
+// checkout
+// log
+
+// ignore
+
+// const a=234;
+// console.log( a )
